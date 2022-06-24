024049913e081d9a5d5dee0aa399fb7a10669677
EvanPlay <99335865+EvanPlay@users.noreply.github.com>
2022-06-24 14:50:46 +0300

    Добавьте файлы проекта.

---
+++ Task_1166/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте. Я - Олег. Программа которая находит цифру, на которую оканчивается число A^B");
+
+            Console.WriteLine("Введите число A");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(a ^ b);
+            int c = (a ^ b)%10;
+            Console.WriteLine("Ответ: ");
+            Console.WriteLine(c);
+        }
+    }
+}
